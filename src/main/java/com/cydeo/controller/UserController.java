5a98f453a5edc59f2a0c923da22a5edb00bc59ec
Human <vladislav.rogovoy@gmail.com>
2024-03-29 17:09:12 -0400

    user create page populated from map data.

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    //inject service
+    UserService userService;
+    RoleService roleService;
+
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
     @GetMapping("/create")
     public String creteUser(Model model){
         model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles", Service.allRoles());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
         return "/user/create";
     }
 }
