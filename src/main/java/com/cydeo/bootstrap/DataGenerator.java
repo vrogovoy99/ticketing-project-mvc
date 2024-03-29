2e3c32e9ec777e1e7e11d6f173ce4d5757685822
Human <vladislav.rogovoy@gmail.com>
2024-03-29 17:02:37 -0400

    user create page populated from map data.

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,7 +1,11 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Gender;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +13,11 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     RoleService roleService; //use interface rather than a class to define variable. Classes can change over time.
+    UserService userService;
 
-    public DataGenerator(RoleService roleService) { //since Component RoleServiceImpl is created, spring will plug it in for RoleService interface - loose coupling!!!!
+    public DataGenerator(RoleService roleService, UserService userService) { //since Component RoleServiceImpl is created, spring will plug it in for RoleService interface - loose coupling!!!!
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @Override
@@ -24,6 +30,45 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(adminRole);
         roleService.save(managerRole);
         roleService.save(employeeRole);
+
+        //add data to user map
+        UserDTO user1 = new UserDTO("John", "Kesy",
+                "john@cydeo.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
+        UserDTO user5 = new UserDTO("Mike", "Smith",
+                "mike@cydeo.com", "Abc2", true, "7459684532", adminRole, Gender.MALE);
+        UserDTO user2 = new UserDTO("Delisa",
+                "Norre", "delisa@cydeo.com", "123", true, "8567412358", managerRole, Gender.FEMALE);
+        UserDTO user3 = new UserDTO("Craig", "Jark",
+                "craig@cydeo.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
+        UserDTO user4 = new UserDTO("Shaun",
+                "Hayns", "shaun@cydeo.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
+        UserDTO user6 = new UserDTO("Elizebeth",
+                "Loren", "elizebeth@cydeo.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Maria",
+                "Ada", "maria@cydeo.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
+        UserDTO user8 = new UserDTO("Bill",
+                "Matt", "bill@cydeo.com", "Abc4", true, "8881239846", employeeRole, Gender.MALE);
+
+        UserDTO user9 = new UserDTO(new Faker().name().firstName(),
+                new Faker().name().lastName(), new Faker().color() + "@cydeo.com", new Faker().avatar().toString(), true, "111111111", employeeRole, Gender.MALE);
+
+        userService.save(user1);
+        userService.save(user2);
+        userService.save(user3);
+        userService.save(user4);
+        userService.save(user5);
+        userService.save(user6);
+        userService.save(user7);
+        userService.save(user8);
+        userService.save(user9);
+        //add employees
+        for(int i=10; i<19;i++){
+            userService.save(
+                    new UserDTO(new Faker().name().firstName(),
+                            new Faker().name().lastName(), new Faker().color() + "@cydeo.com", new Faker().avatar().toString(), true,  "111111111", employeeRole, Gender.MALE)
+            );
+        }
+
 
 
 
