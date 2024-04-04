2b0efc8042ebebac7cf19d543951a86a9dc7e43a
Human <vladislav.rogovoy@gmail.com>
2024-04-04 14:04:45 -0400

    delete user function project objects  completed

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,7 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.impl.AbstractMapService;
+
+public interface ProjectService extends CrudService<ProjectDTO, String> {
+}
