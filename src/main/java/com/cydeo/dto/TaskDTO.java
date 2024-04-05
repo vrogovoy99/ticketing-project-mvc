affdbdd3c6001521aaa99f0d0fbec49a9159d462
Human <vladislav.rogovoy@gmail.com>
2024-04-05 06:15:10 -0400

    task functionality initialized

---
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -0,0 +1,24 @@
+package com.cydeo.dto;
+
+
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TaskDTO {
+    private Long id;
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate assignedDate;
+    private Status status;
+}
